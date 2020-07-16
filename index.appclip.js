8fd3e2f104e0a179678f181f57c92e8c1819c1fe
Tomasz Kania-Orzeł <thomas@MBP-Tomasz.lan>
2020-07-16 12:15:22 +0200

    feat(js): changes in app clip js code

--- index.appclip.js
+++ index.appclip.js
@@ -1,9 +1,13 @@
-import {AppRegistry, View, Text, Button} from 'react-native';
+import {AppRegistry, View, Text} from 'react-native';
 import React from 'react';
 const AppClip = () => (
-  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text style={{fontSize: 22}}>React Native App Clip</Text>
-    <Button title="Learn More" color="#841584" />
+  <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    <Text>App Clip</Text>
   </View>
 );
 
